42787290b0bb6b443c747bedfa89e0d4a0cba898
Panouklakos <captainpanos82@gmail.com>
2023-07-26 17:16:32 +0300

    More bath research done

--- resources/[tools]/research/src/client/stuff/hostels.ts
+++ resources/[tools]/research/src/client/stuff/hostels.ts
@@ -41,19 +41,18 @@
 
 import { Delay } from '@lib/functions';
 import { Vector3 } from '@lib/math';
+import * as net from 'net';
 
 let volume1 = -1;
 let volume2 = -1;
 let particles = -1;
+let introAnimScene = -1;
+let allParticles: number[] = [];
 
-RegisterCommand(
-  'createVolumes',
-  () => {
-    volume1 = CreateVolumeBox(-324.5964, 774.3613, 117.5638, 0.0, 0.0, 10.75094, 1.171861, 1.219162, 2.336554);
-    volume2 = CreateVolumeBox(-319.7882, 762.5787, 117.8969, 0.0, 0.0, 10.09051, 3.165397, 3.414144, 3.011062);
-  },
-  false,
-);
+const CreateVolumes = () => {
+  volume1 = CreateVolumeBox(-324.5964, 774.3613, 117.5638, 0.0, 0.0, 10.75094, 1.171861, 1.219162, 2.336554);
+  volume2 = CreateVolumeBox(-319.7882, 762.5787, 117.8969, 0.0, 0.0, 10.09051, 3.165397, 3.414144, 3.011062);
+};
 
 RegisterCommand(
   'startParticles',
@@ -89,7 +88,47 @@ const LoadAnimationDicts = async () => {
 
     await Delay(5);
   }
+  RequestClipSet('CLIPSET@MINI_GAMES@BATHING@REGULAR@ARTHUR');
+  while (!HasClipSetLoaded('CLIPSET@MINI_GAMES@BATHING@REGULAR@ARTHUR')) {
+    console.log('loadin clipset', HasClipSetLoaded('CLIPSET@MINI_GAMES@BATHING@REGULAR@ARTHUR'));
+
+    await Delay(5);
+  }
+  if (!PrepareSoundset('BATHING_Sounds', false)) {
+    console.log('loadin soundset', PrepareSoundset('BATHING_Sounds', false));
+  }
+  // @ts-ignore
+  while (!RequestScriptAudioBank('BATHING')) {
+    // @ts-ignore
+    console.log('loadin audio bank', RequestScriptAudioBank('BATHING'));
+
+    await Delay(5);
+  }
 };
+// { PlayerPedId(), "Script_Mini_Game_Bathing_Regular", `CLIPSET@MINI_GAMES@BATHING@REGULAR@ARTHUR`, `DEFAULT`, "BATHING" }
+
+// taken from https://github.com/outsider31000/public-scripts/blob/97958b854309be3951e2d743a971624cbc3bd693/server-data/resources/%5Bothers%5D/redm_bathing/structs.js#L4
+const TASK_MOVE_NETWORK_BY_NAME_WITH_INIT_PARAMS = (
+  ped: number,
+  networkdef: string,
+  clipSet: number,
+  p3: number,
+  name: string,
+) => {
+  const struct = new DataView(new ArrayBuffer(512));
+  struct.setBigInt64(0, BigInt(clipSet), true);
+  struct.setBigInt64(8, BigInt(p3), true);
+  struct.setBigInt64(240, BigInt(VarString(10, 'LITERAL_STRING', name)), true);
+
+  Citizen.invokeNative('0x139805C2A67C4795', ped, networkdef, struct, 1.0, 0, 0, 0);
+};
+
+const SetScrubFreq = (freq: number) => {
+  SetTaskMoveNetworkSignalFloat(PlayerPedId(), 'scrub_freq', freq);
+};
+
+//bath_orbit_cam αυτο εδω ειναι το dictionary
+//BATH_ORBIT_REQUEST
 
 let proxyIndex = -1;
 
@@ -116,26 +155,25 @@ RegisterCommand(
   'intro_animation',
   async () => {
     await LoadAnimationDicts();
+    ReserveAmbientPeds(2);
     console.log('Starting music event');
     TriggerMusicEvent('MG_BATHING_START');
     console.log('Done loading animation dictionaries');
-    const animScene = CreateAnimScene(
+    introAnimScene = CreateAnimScene(
       'script@mini_game@bathing@BATHING_INTRO_OUTRO_VALENTINE',
       0,
-      'Pbl_regular_intro',
+      's_Regular_intro',
       false,
       true,
     );
-    console.log('Created anim scene:', animScene);
-    SetAnimSceneEntity(animScene, 'ARTHUR', PlayerPedId(), 0);
-    // SetAnimSceneEntity(animScene, 'john_Marston', PlayerPedId(), 0);
-    LoadAnimScene(animScene);
+    console.log('Created anim scene:', introAnimScene);
+    SetAnimSceneEntity(introAnimScene, 'ARTHUR', PlayerPedId(), 0);
+    // SetAnimSceneEntity(introAnimScene, 'john_Marston', PlayerPedId(), 0);
+    LoadAnimScene(introAnimScene);
     await Delay(1000);
-    StartAnimScene(animScene);
+    StartAnimScene(introAnimScene);
     EnableArtificalLightsForThisInterior();
-    await Delay(15000);
-    console.log('Stopping music event');
-    TriggerMusicEvent('MG_BATHING_STOP');
+    CreateVolumes();
   },
   false,
 );
@@ -151,7 +189,7 @@ RegisterCommand(
     const animScene = CreateAnimScene(
       'script@mini_game@bathing@BATHING_INTRO_OUTRO_VALENTINE',
       0,
-      'Pbl_regular_outro',
+      's_Regular_outro',
       false,
       true,
     );
@@ -164,20 +202,91 @@ RegisterCommand(
     if (proxyIndex !== -1) {
       Citizen.invokeNative('0xBFCB17895BB99E4E', proxyIndex, false);
     }
+    if (DoesVolumeExist(volume1)) {
+      DeleteVolume(volume1);
+    }
+    if (DoesVolumeExist(volume2)) {
+      DeleteVolume(volume2);
+    }
   },
   false,
 );
 
 RegisterCommand(
-  'in_bath',
-  () => {
-    Citizen.invokeNative('0xB8B207C34285E978', 'bath_orbit_cam');
-    Citizen.invokeNative('0xAC77757C05DE9E5A', 'bath_orbit_cam');
-    // Citizen.invokeNative('0x6A4D224FC7643941', 'bath_orbit_cam'); // LoadCameraDataDict
+  'inside_bath',
+  async () => {
+    ExecuteCommand('intro_animation');
+    while (!IsAnimSceneFinished(introAnimScene, true)) {
+      await Delay(5);
+    }
+    SetPedCanLegIk(PlayerPedId(), false);
+    SetPedLegIkMode(PlayerPedId(), 0);
+    // Citizen.invokeNative('0x69D65E89FFD72313', true, true);
+    SetScrubFreq(0.0);
+    TASK_MOVE_NETWORK_BY_NAME_WITH_INIT_PARAMS(
+      PlayerPedId(),
+      'Script_Mini_Game_Bathing_Regular',
+      GetHashKey('CLIPSET@MINI_GAMES@BATHING@REGULAR@ARTHUR'),
+      GetHashKey('DEFAULT'),
+      'BATHING',
+    );
+    ForcePedAiAndAnimationUpdate(PlayerPedId(), false, false);
+    Citizen.invokeNative('0x55546004A244302A', PlayerPedId());
+
+    await Delay(5000);
+
+    while (!IsTaskMoveNetworkReadyForTransition(PlayerPedId())) {
+      console.log('Waiting for the clipset to be ready for network transitions');
+      await Delay(5);
+    }
+
+    console.log('Ready for transitions');
+
+    if (GetTaskMoveNetworkState(PlayerPedId()) !== 'Scrub_Idle') {
+      RequestTaskMoveNetworkStateTransition(PlayerPedId(), 'Scrub_Idle');
+      console.log('Should start to transition to scrub idle');
+      await Delay(5000);
+    }
+
+    while (!IsTaskMoveNetworkReadyForTransition(PlayerPedId())) {
+      console.log('Waiting for the clipset to be ready for network transitions');
+      await Delay(5);
+    }
+
+    SetScrubFreq(0.75);
+
+    if (GetTaskMoveNetworkState(PlayerPedId()) !== 'Scrub_Right_Leg') {
+      const timeStarted = GetCloudTimeAsInt();
+      console.log('Should start to transition to right leg');
+
+      while (GetCloudTimeAsInt() - timeStarted < 15000) {
+        console.log(GetCloudTimeAsInt() - timeStarted);
+        if (GetTaskMoveNetworkState(PlayerPedId()) !== 'Scrub_Right_Leg') {
+          RequestTaskMoveNetworkStateTransition(PlayerPedId(), 'Scrub_Right_Leg');
+        }
+        await Delay(1);
+      }
+    }
+
+    // const particleHandle = StartParticleFxLoopedOnPedBone(
+    //   'scr_mg_bathing_foam_head',
+    //   PlayerPedId(),
+    //   0.0,
+    //   0.0,
+    //   0.0,
+    //   0.0,
+    //   0.0,
+    //   0.0,
+    //   21030,
+    //   0.4,
+    //   false,
+    //   false,
+    //   false,
+    // );
+    // allParticles.push(particleHandle);
   },
   false,
 );
-
 on('onResourceStop', (resource: string) => {
   if (resource !== GetCurrentResourceName()) return;
   if (DoesVolumeExist(volume1)) {
@@ -195,4 +304,12 @@ on('onResourceStop', (resource: string) => {
   if (proxyIndex !== -1) {
     Citizen.invokeNative('0xBFCB17895BB99E4E', proxyIndex, false);
   }
+  ClearPedTasks(PlayerPedId());
+  Citizen.invokeNative('0x69D65E89FFD72313', false, false);
+  Citizen.invokeNative('0x77ED170667F50170', 'BATHING');
+  SetPedCanLegIk(PlayerPedId(), true);
+  SetPedLegIkMode(PlayerPedId(), 2);
+  allParticles.forEach((particle) => {
+    RemoveParticleFx(particle, true);
+  });
 });
